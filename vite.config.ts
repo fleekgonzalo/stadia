35c21a8d8f4cdf501b1e77586733c9c1aee4d9db
damalpol <122043310+damalpol@users.noreply.github.com>
2024-07-01 13:50:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],  
+  plugins: [react()],
+  build: {
+    output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'my-library.[ext][hash].js',
+    },
+  },
 })
